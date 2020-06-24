1a527f60fbbc61a7647137d4a2bd1885f0cdaddd
yogesh25 <yogesh.tayal@inspopindia.com>
2020-06-24 22:59:55 +0530

    Pushing DashBoard shared TC after resolving conflicts

--- src/ApplicationPages/DashBoard.java
+++ src/ApplicationPages/DashBoard.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.support.FindBy;
 public class DashBoard {
 
     //	LET THE SEQUENCE ALWAYS REMAIN THE SAME AS THEY APPEAR ON THE WEBPAGE.
+	
+	public String maximizeVehicleDetail = "//*[contains(@id,'{0}')]";//{0}->2012-AUDI-A8LQUATTRO
 
     @FindBy(id="btnGetIdCards-Dashboard")
     public WebElement btn_GetIdCards;
 
     @FindBy(id="btnMakeAPayment-Dashboard")
-    public WebElement btn_MaanagePayment;
+    public WebElement btn_ManagePayment;
 
     @FindBy(id="btnFileACliam-Dashboard")
     public WebElement btn_FileACliam;
@@ -34,6 +36,29 @@ public class DashBoard {
     @FindBy(id="btnAddReplaceVehicle")
     public WebElement btn_AddReplaceVehicle;
 
+    @FindBy(xpath="//a[contains(text(),'Policy')]")
+    public WebElement btn_Policy;
+    
+    @FindBy(xpath="//a[contains(text(),'Documents')]")
+    public WebElement btn_Documents;
+    
+    @FindBy(xpath="//a[contains(text(),'Claims')]")
+    public WebElement btn_Claims;
+    
+    @FindBy(xpath="//a[contains(text(),'Personal Info')]")
+    public WebElement btn_PersonalInfo;
+    
+    @FindBy(id="btnReschedule")
+    public WebElement btn_Reschedule;
+    
+    @FindBy(xpath="//span[text()='Edit']")
+	public WebElement btn_EditVehicle;
+	 
+	@FindBy(xpath="//span[text()='Get ID Card']")
+	public WebElement btn_IDCardForVehicle;
+	
+	 @FindBy(xpath="//span[(text()='Remove')]")
+	    public WebElement btn_RemoveVehicle;
 
 
 
