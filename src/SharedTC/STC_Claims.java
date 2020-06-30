4ae7dc2bd769e1b6dc4225c11b6bd648cbe920d8
dkumar0104 <dkumar0104@gmail.com>
2020-06-30 16:23:27 +0530

    scripts update

--- src/SharedTC/STC_Claims.java
+++ src/SharedTC/STC_Claims.java
@@ -1,18 +1,24 @@
 package SharedTC;
 
 import ApplicationPages.Claims;
+
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import resources.Testing;
 
 public class STC_Claims {
-    public void claims(Testing test){
+    public void verifyClaims(Testing test){
         test.setPage(Claims.class);
         Claims claims = (Claims) PageFactory.initElements(test.driver, test.getPage());
-        test.webFunctions().readInfo(test, claims.text_ClaimNumber);
-        test.webFunctions().readInfo(test, claims.text_LossDate);
-        test.webFunctions().readInfo(test, claims.text_ClaimStatus);
+        String ClaimNo = test.webFunctions().readInfo(test, claims.text_ClaimNumber);
+        System.out.println(ClaimNo);
+        String LossDate = test.webFunctions().readInfo(test, claims.text_LossDate);
+        System.out.println(LossDate);
+        String ClaimStatus = test.webFunctions().readInfo(test, claims.text_ClaimStatus);
+        System.out.println(ClaimStatus);
         test.webFunctions().click(test, claims.btn_ViewClaimDetails);
-        test.webFunctions().click(test, claims.btn_FileClaim);
+       
+       
     }
 
 }
